10c4948a189243b84bf6b62ec5dd600f416ccd24
Shalom Quek <shalomquek@gmail.com>
2022-08-05 16:12:13 +0800

    Added components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import Hello from './components/Hello'
+import Message from './components/Message'
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Functional component */}
+      <Greet />
+      {/* Class compoment */}
+      <Welcome name="Class"/>
+      {/* JSX */}
+      <Hello name="Mimi">
+        <p>Watagi?</p>
+      </Hello>
+      <Message />
+      <Counter />
     </div>
   );
 }
